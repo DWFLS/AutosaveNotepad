ade35215091fbce8f8a84d640dd1f5ec2d9ada73
DWFelus <77146903+DWFelus@users.noreply.github.com>
2022-05-11 17:04:01 +0200

    Added New, Open and Save As.

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ namespace AutosaveNotepad
 {
     public partial class formMain : Form
     {
+        string currentFileName;
 
         public formMain()
         {
@@ -17,21 +18,29 @@ namespace AutosaveNotepad
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            SaveAs("New...", true);
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open";
+            openFileDialog.Filter = "Text Document|*.txt|All Files|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+            }
+            this.Text = "AutosaveNotepad - " + openFileDialog.FileName;
+            currentFileName = openFileDialog.FileName;
         }
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            SaveAs("New...", false);
         }
 
         private void saveAsCopyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            // quick save and promptless load of currentFileName
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -40,6 +49,23 @@ namespace AutosaveNotepad
         }
 
         // FILE functions
+
+        private void SaveAs(string title, bool clear)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = title;
+            saveFileDialog.Filter = "Text Document|*.txt|All Files|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (clear)
+                {
+                    richTextBox.Clear();
+                }
+                richTextBox.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+            }
+            this.Text = "AutosaveNotepad - " + saveFileDialog.FileName;
+            currentFileName = saveFileDialog.FileName;
+        }
 
         private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -50,14 +76,6 @@ namespace AutosaveNotepad
         {
 
         }
-
-
-
-        void Save(string filename)
-        {
-
-        }
-
 
         // EDIT menu
 
