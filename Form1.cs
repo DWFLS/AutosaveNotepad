dd8bb4a42e40d89043745fa4a8e0c0efb04ce47f
DWFelus <77146903+DWFelus@users.noreply.github.com>
2023-06-02 16:26:32 +0200

    1.0.6.0.1 - toolstrip moved

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,6 @@
 namespace AutosaveNotepad
 {
     using System;
-    using System.Diagnostics;
     using System.Drawing;
     using System.Drawing.Printing;
     using System.IO;
@@ -895,40 +894,7 @@ namespace AutosaveNotepad
 
         #region TOOL STRIP bar
 
-        //
-        // TOOL STRIP bar
-        //
 
-        private void editToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void statusStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
-
-        }
-
-        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
-        {
-            string url = "https://www." + toolStripStatusLabel2.Text;
-            ProcessStartInfo psi = new ProcessStartInfo
-            {
-                FileName = url,
-                UseShellExecute = true
-            };
-            Process.Start(psi);
-        }
-
-        private void toolStripStatusLabel1_Click_1(object sender, EventArgs e)
-        {
-
-        }
 
         #endregion
 
---
+++ ToolStrip.cs
@@ -0,0 +1,43 @@
+﻿namespace AutosaveNotepad
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Forms;
+    public partial class formMain : Form
+    {
+        //
+        // TOOL STRIP bar
+        //
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            string url = "https://www." + toolStripStatusLabel2.Text;
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+
+        private void toolStripStatusLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
